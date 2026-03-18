bc79e14bd4fa080c19c9d2b56bca634c4de264ea
Lord-Lucius <luluzuri@student.42.fr>
2026-03-18 20:06:59 +0100

    Need to a good split of words before using regex

--- srcs/lexer/lexer.cpp
+++ srcs/lexer/lexer.cpp
@@ -4,16 +4,23 @@
 
 #include "lexer/lexer.hpp"
 
+std::vector<std::string> split_code(const std::string &code) {
+	std::vector<std::string> words_vector;
+
+	
+}
+
 std::vector<Token> tokenizer(const std::string &code) {
 	std::vector<Token> vector_token;
 	std::stringstream ss(code);
 	std::string word;
 
 	// regex part
-	std::regex identifier_pattern("[a-zA-z_]\\w*");
+	std::regex identifier_pattern("^[a-zA-Z_]\\w*$");
 	while (ss >> word) {
+		//std::cout << i << ": " << word << std::endl;
 		if (std::regex_match(word, identifier_pattern)) {
-			std::cout << word << std::endl;
+			std::cout << "In if condition: " << word << std::endl;
 		}
 	}
 	return (vector_token);
