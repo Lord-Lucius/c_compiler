c60c4001ce3b877505a065923a95be416729b925
luluzuri <luluzuri@student.42.fr>
2026-04-11 20:25:50 +0200

    feature[]: Working and changes the base of the parser to do a complete one

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -1,16 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Parser.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/07 23:00:21 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/04/07 23:00:25 by luluzuri         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-
 #pragma once
 
 #include <string>
@@ -19,241 +6,255 @@
 #include "Lexer.hpp"
 
 class ASTNode {
-	public:
-		virtual ~ASTNode(void) {}
+public:
+    virtual ~ASTNode(void) {}
 };
 
 class Expression : public ASTNode {
-	public:
-		~Expression(void) override {}
+public:
+    ~Expression(void) override {}
 };
 
 class Statement : public ASTNode {
-	public:
-		~Statement(void) override {}
+public:
+    ~Statement(void) override {}
 };
 
 class Type : public ASTNode {
-	private:
-		std::string _name;
+private:
+    std::string _name;
 
-	public:
-		Type(const std::string &name) : _name(name) {}
-		~Type(void) override {}
+public:
+    Type(const std::string &name) : _name(name) {}
+    ~Type(void) override {}
 
-		std::string getName(void) const { return _name; }
+    std::string getName(void) const { return _name; }
 };
 
 class Identifier : public Expression {
-	private:
-		std::string _name;
+private:
+    std::string _name;
 
-	public:
-		Identifier(const std::string &n) : _name(n) {}
-		std::string getName(void) const { return _name; }
+public:
+    Identifier(const std::string &n) : _name(n) {}
+    std::string getName(void) const { return _name; }
 };
 
 class Constant : public Expression {
-	private:
-		int _value;
+private:
+    int _value;
 
-	public:
-		Constant(int val) : _value(val) {}
-		int getValue(void) const { return _value; }
+public:
+    Constant(int val) : _value(val) {}
+    int getValue(void) const { return _value; }
 };
 
 class BinOp : public Expression {
-	private:
-		Expression *_left;
-		std::string _op;
-		Expression *_right;
+private:
+    Expression *_left;
+    std::string _op;
+    Expression *_right;
 
-	public:
-		BinOp(Expression *l, const std::string &op, Expression *r)
-			: _left(l), _op(op), _right(r) {}
+public:
+    BinOp(Expression *l, const std::string &op, Expression *r)
+        : _left(l), _op(op), _right(r) {}
 
-		Expression *getLeft(void) const { return _left; }
-		std::string getOp(void) const { return _op; }
-		Expression *getRight(void) const { return _right; }
+    Expression *getLeft(void) const { return _left; }
+    std::string getOp(void) const { return _op; }
+    Expression *getRight(void) const { return _right; }
 };
 
 class UnaryOp : public Expression {
-	private:
-		std::string _op;
-		Expression *_expr;
+private:
+    std::string _op;
+    Expression *_expr;
 
-	public:
-		UnaryOp(const std::string &op, Expression *expr)
-			: _op(op), _expr(expr) {}
+public:
+    UnaryOp(const std::string &op, Expression *expr)
+        : _op(op), _expr(expr) {}
 
-		std::string getOp(void) const { return _op; }
-		Expression *getExpr(void) const { return _expr; }
+    std::string getOp(void) const { return _op; }
+    Expression *getExpr(void) const { return _expr; }
 };
 
 class Assignment : public Expression {
-	private:
-		std::string _name;
-		Expression *_value;
+private:
+    std::string _name;
+    Expression *_value;
 
-	public:
-		Assignment(const std::string &name, Expression *val)
-			: _name(name), _value(val) {}
+public:
+    Assignment(const std::string &name, Expression *val)
+        : _name(name), _value(val) {}
 
-		std::string getName(void) const { return _name; }
-		Expression *getValue(void) const { return _value; }
+    std::string getName(void) const { return _name; }
+    Expression *getValue(void) const { return _value; }
 };
 
 class Return : public Statement {
-	private:
-		Expression *_value;
+private:
+    Expression *_value;
 
-	public:
-		Return(Expression *val) : _value(val) {}
-		Expression *getValue(void) const { return _value; }
+public:
+    Return(Expression *val) : _value(val) {}
+    Expression *getValue(void) const { return _value; }
 };
 
 class Block : public Statement {
-	private:
-		std::vector<Statement *> _statements;
+private:
+    std::vector<Statement *> _statements;
 
-	public:
-		Block(const std::vector<Statement *> &stmts)
-			: _statements(stmts) {}
+public:
+    Block(const std::vector<Statement *> &stmts) : _statements(stmts) {}
 
-		std::vector<Statement *> getStatements(void) const {
-			return _statements;
-		}
+    std::vector<Statement *> getStatements(void) const { return _statements; }
 };
 
 class If : public Statement {
-	private:
-		Expression *_condition;
-		Statement *_thenBranch;
-		Statement *_elseBranch;
+private:
+    Expression *_condition;
+    Statement *_thenBranch;
+    Statement *_elseBranch;
 
-	public:
-		If(Expression *cond, Statement *thenB, Statement *elseB)
-			: _condition(cond), _thenBranch(thenB), _elseBranch(elseB) {}
+public:
+    If(Expression *cond, Statement *thenB, Statement *elseB)
+        : _condition(cond), _thenBranch(thenB), _elseBranch(elseB) {}
 
-		Expression *getCondition(void) const { return _condition; }
-		Statement *getThen(void) const { return _thenBranch; }
-		Statement *getElse(void) const { return _elseBranch; }
+    Expression *getCondition(void) const { return _condition; }
+    Statement *getThen(void) const { return _thenBranch; }
+    Statement *getElse(void) const { return _elseBranch; }
 };
 
 class While : public Statement {
-	private:
-		Expression *_condition;
-		Statement *_body;
+private:
+    Expression *_condition;
+    Statement *_body;
 
-	public:
-		While(Expression *cond, Statement *body)
-			: _condition(cond), _body(body) {}
+public:
+    While(Expression *cond, Statement *body)
+        : _condition(cond), _body(body) {}
 
-		Expression *getCondition(void) const { return _condition; }
-		Statement *getBody(void) const { return _body; }
+    Expression *getCondition(void) const { return _condition; }
+    Statement *getBody(void) const { return _body; }
 };
 
 class VarDecl : public Statement {
-	private:
-		Type *_type;
-		std::string _name;
-		Expression *_init;
+private:
+    Type *_type;
+    std::string _name;
+    Expression *_init;
 
-	public:
-		VarDecl(Type *t, const std::string &n, Expression *init)
-			: _type(t), _name(n), _init(init) {}
+public:
+    VarDecl(Type *t, const std::string &n, Expression *init = nullptr)
+        : _type(t), _name(n), _init(init) {}
 
-		Type *getType(void) const { return _type; }
-		std::string getName(void) const { return _name; }
-		Expression *getInit(void) const { return _init; }
+    Type *getType(void) const { return _type; }
+    std::string getName(void) const { return _name; }
+    Expression *getInit(void) const { return _init; }
 };
 
 class ExprStmt : public Statement {
-	private:
-		Expression *_expr;
+private:
+    Expression *_expr;
 
-	public:
-		ExprStmt(Expression *expr) : _expr(expr) {}
-		Expression *getExpr(void) const { return _expr; }
+public:
+    ExprStmt(Expression *expr) : _expr(expr) {}
+    Expression *getExpr(void) const { return _expr; }
+};
+
+class Parameter {
+private:
+    Type *_type;
+    std::string _name;
+
+public:
+    Parameter(Type *type, const std::string &name)
+        : _type(type), _name(name) {}
+
+    Type *getType(void) const { return _type; }
+    std::string getName(void) const { return _name; }
 };
 
 class Function {
-	private:
-		Type *_return_type;
-		std::string _name;
-		std::vector<Statement *> _body;
+private:
+    Type *_return_type;
+    std::string _name;
+    std::vector<Parameter *> _parameters;
+    Block *_body;
 
-	public:
-		Function(Type *rt, const std::string &n,
-				 const std::vector<Statement *> &b)
-			: _return_type(rt), _name(n), _body(b) {}
+public:
+    Function(Type *rt, const std::string &n,
+             const std::vector<Parameter *> &params,
+             Block *body)
+        : _return_type(rt), _name(n), _parameters(params), _body(body) {}
 
-		Type *getReturnType(void) const { return _return_type; }
-		std::string getName(void) const { return _name; }
-		std::vector<Statement *> getBody(void) const { return _body; }
+    Type *getReturnType(void) const { return _return_type; }
+    std::string getName(void) const { return _name; }
+    std::vector<Parameter *> getParameters(void) const { return _parameters; }
+    Block *getBody(void) const { return _body; }
 };
 
 class Program {
-	private:
-		std::vector<Function *> _functions;
+private:
+    std::vector<Function *> _functions;
 
-	public:
-		void addFunction(Function *f) { _functions.push_back(f); }
-
-		std::vector<Function *> getFunctions(void) const {
-			return _functions;
-		}
+public:
+    void addFunction(Function *f) { _functions.push_back(f); }
+    std::vector<Function *> getFunctions(void) const { return _functions; }
 };
 
 class Parser {
-	private:
-		std::vector<Token *> _tokens;
-		int _current = 0;
+private:
+    std::vector<Token *> _tokens;
+    int _current = 0;
 
-		Token *peek(void);
-		Token *advance(void);
-		Token *previous(void);
-		bool isAtEnd(void);
+    Token *peek(void);
+    Token *advance(void);
+    Token *previous(void);
+    bool isAtEnd(void);
 
-		bool check(TokenType type);
-		bool check(Keyword kw);
-		bool check(Operator op);
-		bool check(Punctuation p);
+    bool check(TokenType type);
+    bool check(Keyword kw);
+    bool check(Operator op);
+    bool check(Literal lit);
+    bool check(Punctuation p);
 
-		bool match(TokenType type);
-		bool match(Keyword kw);
-		bool match(Operator op);
-		bool match(Punctuation p);
+    bool match(TokenType type);
+    bool match(Keyword kw);
+    bool match(Operator op);
+    bool match(Literal lit);
+    bool match(Punctuation p);
 
-		Token *expect(TokenType type, const std::string &msg);
-		Token *expect(Keyword kw, const std::string &msg);
-		Token *expect(Punctuation p, const std::string &msg);
+    Token *expect(TokenType type, const std::string &msg);
+    Token *expect(Keyword kw, const std::string &msg);
+    Token *expect(Literal lit, const std::string &msg);
+    Token *expect(Punctuation p, const std::string &msg);
 
-		Program *parseProgram(void);
-		Function *parseFunction(void);
-		Type *parseType(void);
+    Program *parseProgram(void);
+    Function *parseFunction(void);
+    Parameter *parseParameter(void);
+    Type *parseType(void);
 
-		Statement *parseStatement(void);
-		Statement *parseBlock(void);
-		Statement *parseIf(void);
-		Statement *parseWhile(void);
-		Statement *parseReturn(void);
-		Statement *parseVarDecl(void);
+    Block *parseBlock(void);
+    Statement *parseStatement(void);
+    Statement *parseReturn(void);
+    Statement *parseIf(void);
+    Statement *parseWhile(void);
+    Statement *parseVarDecl(void);
+    Statement *parseExprStmt(void);
 
-		Expression *parseExpression(void);
-		Expression *parseAssignment(void);
-		Expression *parseEquality(void);
-		Expression *parseComparison(void);
-		Expression *parseTerm(void);
-		Expression *parseFactor(void);
-		Expression *parseUnary(void);
-		Expression *parsePrimary(void);
+    Expression *parseExpression(void);
+    Expression *parseAssignment(void);
+    Expression *parseEquality(void);
+    Expression *parseComparison(void);
+    Expression *parseTerm(void);
+    Expression *parseFactor(void);
+    Expression *parseUnary(void);
+    Expression *parsePrimary(void);
 
-	public:
-		Parser(std::vector<Token *> tokens)
-			: _tokens(tokens) {}
+public:
+    Parser(const std::vector<Token *> &tokens) : _tokens(tokens) {}
 
-		Program *parse(void) {
-			return parseProgram();
-		}
+    Program *parse(void) {
+        return parseProgram();
+    }
 };
--- srcs/parser/Parser.cpp
+++ srcs/parser/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 01:35:55 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/04/07 23:16:17 by luluzuri         ###   ########.fr       */
+/*   Updated: 2026/04/11 20:25:27 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,58 @@
 #include "Lexer.hpp"
 #include "Exceptions.hpp"
 #include <cstddef>
+#include <vector>
 
 /* STARTING PARSING IN BRANCH */
-/* Need to do a plan of ASDL and AST that i want use for the parsing */
-
-Token *Parser::peek(void) { return (_tokens[_current]); }
+Token *Parser::peek(void) {
+	if (isAtEnd())
+		return nullptr;
+	return _tokens[_current];
+}
 
 Token *Parser::advance(void) {
-	Token *tmp = _tokens[_current];
-	_current++;
-	return (tmp);
+	if (!isAtEnd())
+		_current++;
+	return (previous());
 }
 
-Token *Parser::previous(void) { return (_tokens[_current - 1]); }
+Token *Parser::previous(void) {
+	return _tokens[_current - 1];
+}
 
-bool Parser::check(enum TokenType type) {
-	if (isAtEnd())
+bool Parser::isAtEnd(void) {
+	return (_current >= _tokens.size());
+}
+
+bool Parser::check(TokenType type) {
+	return (_tokens[_current]->TYPE == type);
+}
+
+bool Parser::check(Keyword kw) {
+	if (!check(TokenType::Keyword))
 		return (false);
-	return (peek()->TYPE == type);
+	return (peek()->keyword.value() == kw);
 }
 
-bool Parser::match(enum TokenType type) {
+bool Parser::check(Operator op) {
+	if (!check(TokenType::Operator))
+		return (false);
+	return (peek()->oper.value() == op);
+}
+
+bool Parser::check(Literal lit) {
+	if (!check(TokenType::Literal))
+		return (false);
+	return (peek()->lit.value() == lit);
+}
+
+bool Parser::check(Punctuation punc) {
+	if (!check(TokenType::Punctuation))
+		return (false);
+	return (peek()->punc.value() == punc);
+}
+
+bool Parser::match(TokenType type) {
 	if (check(type)) {
 		advance();
 		return (true);
@@ -42,19 +73,148 @@ bool Parser::match(enum TokenType type) {
 	return (false);
 }
 
-Token *Parser::expect(TokenType type, const std::string &message) {
-	if (check(type))
-		return advance();
-	throw UnexpectedTokenException(message);
+bool Parser::match(Keyword kw) {
+	if (check(kw)) {
+		advance();
+		return (true);
+	}
+	return (false);
 }
 
-bool Parser::isAtEnd(void) { return (_current == _tokens.size()); }
+bool Parser::match(Operator op) {
+	if (check(op)) {
+		advance();
+		return (true);
+	}
+	return (false);
+}
 
-// Parse function part
+bool Parser::match(Literal lit) {
+	if (check(lit)) {
+		advance();
+		return (true);
+	}
+	return (false);
+}
+
+bool Parser::match(Punctuation punc) {
+	if (check(punc)) {
+		advance();
+		return (true);
+	}
+	return (false);
+}
+
+Token *Parser::expect(TokenType type, const std::string &msg) {
+	if (check(type)) {
+		return (advance());
+	}
+	throw UnexpectedTokenException(msg);
+}
+
+Token *Parser::expect(Keyword kw, const std::string &msg) {
+	if (check(kw)) {
+		return (advance());
+	}
+	throw UnexpectedTokenException(msg);
+}
+
+Token *Parser::expect(Literal lit, const std::string &msg) {
+	if (check(lit)) {
+		return (advance());
+	}
+	throw UnexpectedTokenException(msg);
+}
+
+Token *Parser::expect(Punctuation punc, const std::string &msg) {
+	if (check(punc)) {
+		return (advance());
+	}
+	throw UnexpectedTokenException(msg);
+}
+
 Program *Parser::parseProgram(void) {
 	Program *p = new Program();
+
 	while (!isAtEnd()) {
-		p->addFunction(parseFunction());
+		Function *func = parseFunction();
+		p->addFunction(func);
 	}
 	return (p);
 }
+
+Function *Parser::parseFunction(void) {
+	Type *return_type = parseType();
+	std::string name = expect(TokenType::Identifier, "Expected function name")->value;
+	expect(Punctuation::LEFT_PAREN, "Expected '(' and didn't found it");
+	std::vector<Parameter *> parameters;
+	if (!check(Punctuation::RIGHT_PAREN)) {
+		do {
+			Parameter *param = parseParameter();
+			parameters.push_back(param);
+		} while (match(Punctuation::RIGHT_PAREN));
+	}
+	expect(Punctuation::RIGHT_PAREN, "Expected ')'");
+	Block *body = parseBlock();
+
+	return (new Function(return_type, name, parameters, body));
+}
+
+Parameter *Parser::parseParameter(void) {
+	Type *type = parseType();
+	Token *token_name = expect(TokenType::Identifier, "Expected parameter name");
+	std::string name = token_name->value;
+	return (new Parameter(type, name));
+}
+
+Type *Parser::parseType(void) {
+	if (check(Keyword::INT)) {
+		advance();
+		return (new Type("int"));
+	} else if (check(Keyword::VOID)) {
+		advance();
+		return (new Type("void"));
+	} else if (check(Keyword::FLOAT)) {
+		advance();
+		return (new Type("float"));
+	} else if (check(Keyword::CHAR)) {
+		advance();
+		return (new Type("char"));
+	}
+	throw InvalidTypeException("Expected type");
+}
+
+Block *Parser::parseBlock(void) {
+	std::vector<Statement *> statements;
+	expect(Punctuation::LEFT_BRACKET, "Expected '{'");
+	while (!check(Punctuation::RIGHT_BRACKET) && !isAtEnd()) {
+		Statement *s = parseStatement();
+		statements.push_back(s);
+	}
+	expect(Punctuation::RIGHT_BRACKET, "Expected '}'");
+	return (new Block(statements));
+}
+
+Statement *Parser::parseStatement(void) {
+	if (check(Keyword::RETURN))
+		return (parseReturn());
+	else if (check(Keyword::IF))
+		return (parseIf());
+	else if (check(Keyword::WHILE))
+		return (parseWhile());
+	else if (check(Punctuation::LEFT_BRACKET))
+		return (parseBlock());
+	else if (check(Keyword::INT) || check(Keyword::FLOAT) || check(Keyword::CHAR))
+		return (parseVarDecl());
+	else if (check(TokenType::Identifier))
+		return (parseExprStmt());
+	throw ParserException("Expected statement");
+}
+
+Statement *Parser::parseReturn(void) {
+	expect(Keyword::RETURN, "Expected 'retur'");
+	Expression *expr = parseExpression();
+	expect(Punctuation::SEMICOLON, "Expected ';' after return");
+	return (new Return(expr));
+}
+
